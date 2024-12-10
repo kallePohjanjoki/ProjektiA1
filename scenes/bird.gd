1211378e1aabdebd294ced93d5636d54d786f4cd
siirihelisto <siiri.helisto@gmail.com>
2024-12-10 12:41:58 +0200

    godot

--- scenes/bird.gd
+++ scenes/bird.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position.x -= get_parent().speed / 2
